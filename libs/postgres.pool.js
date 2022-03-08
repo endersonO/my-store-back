d20af9c574afaa94868848985b496dc6a0f8c010
enderson Omaña <enderson19952009@gmail.com>
2022-03-08 14:16:31 -0400

    fix error connection

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -3,14 +3,20 @@ const { Pool } = require('pg');
 const { config } = require('./../config/config');
 
 let URI = '';
+const options = {
+  connectionString: URI
+}
 
 if (config.isProd) {
-  URI = config.dbUrl;
+  options.connectionString = config.dbUrl;
+  options.ssl = {rejectUnauthorized : false}
 } else {
   const USER = encodeURIComponent(config.dbUser);
   const PASSWORD = encodeURIComponent(config.dbPassword);
   URI = `postgress://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = config.URI;
 }
-const pool = new Pool({ connectionString: URI });
+
+const pool = new Pool(options);
 
 module.exports = pool;
